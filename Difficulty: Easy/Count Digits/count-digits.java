89bd62b4bb25082b0a3266a77ec4946bb70c0ca1
Pravallika_kola <134603637+21p31a04g2@users.noreply.github.com>
2025-08-04 19:44:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count Digits/count-digits.java
@@ -0,0 +1,15 @@
+//Back-end complete function Template for Java
+import java.util.*;
+
+public class Solution {
+    public static void main(String args[]) {
+        // Your Code Here
+            Scanner sc=new Scanner(System.in);
+            int n=sc.nextInt();
+            double count=Math.log10(n)+1;
+            int res=(int) count;
+            System.out.println(res);
+            
+            
+    }
+}
